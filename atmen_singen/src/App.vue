a658f5e5c89805add9ea8fdec7f26960aed1ae3f
paul.hublitz <paul.hublitz@stud.htwk-leipzig.de>
2025-03-15 14:00:32 +0100

    added header

---
+++ atmen_singen/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header class="header">
+    <img src=" @/assets/primär_mS-24.svg" alt="HeaderLogo">
+  </header>
+
+
+  <RouterView />
+</template>
+
+<style scoped>
+.header {
+  display: block;
+  margin: auto;
+  width: 20%;
+}
+</style>
