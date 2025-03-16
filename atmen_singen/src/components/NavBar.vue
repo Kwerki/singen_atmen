4d2948a36c678944e203e61ca9c2f7455b4be7b1
Kwerki <paulhublitz@googlemail.com>
2025-03-16 12:34:59 +0100

    added navigation drawer

--- atmen_singen/src/components/NavBar.vue
+++ atmen_singen/src/components/NavBar.vue
@@ -1,42 +1,86 @@
 <template>
-    <div class="navbar-wrapper">
-        <v-app-bar class="navbar" :elevation="0" color="white">
 
-            <v-btn :ripple="false" class="home-btn" icon variant="plain" to="/home">
-                <img src="@/assets/home_button.svg" alt="Home" class="home-logo">
-            </v-btn>
+    <v-app>
+        <div class="navbar-wrapper">
+            <v-app-bar class="navbar" :elevation="0" color="white">
+
+                <v-btn :ripple="false" class="home-btn" icon variant="plain" to="/home">
+                    <img src="@/assets/home_button.svg" alt="Home" class="home-logo">
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-container class="d-flex justify-center">
+                    <div class="d-flex button-group">
+                        <v-btn :ripple="false" variant="plain" to="/atmen"
+                            :class="{ 'active-route': $route.path === '/atmen' }">
+                            <img src="@/assets/navbar_logo_atmen.svg" width="24" height="24">
+                            <div class="btn-text">Atmen</div>
+                        </v-btn>
+                        <v-btn :ripple="false" variant="plain" to="/sprechen"
+                            :class="{ 'active-route': $route.path === '/sprechen' }">
+                            <img src="@/assets/navbar_logo_sprechen.svg" width="24" height="24">
+                            <div class="btn-text">Sprechen</div>
+                        </v-btn>
+                        <v-btn :ripple="false" variant="plain" to="/singen"
+                            :class="{ 'active-route': $route.path === '/singen' }">
+                            <img src="@/assets/navbar_logo_singen.svg" width="24" height="24">
+                            <div class="btn-text">Singen</div>
+                        </v-btn>
+                        <v-btn :ripple="false" variant="plain" to="/klavier"
+                            :class="{ 'active-route': $route.path === '/klavier' }">
+                            <img src="@/assets/navbar_logo_klavier.svg" width="24" height="24">
+                            <div class="btn-text">Klavier</div>
+                        </v-btn>
+                    </div>
+                </v-container>
+
+                <v-spacer></v-spacer>
+
+                <v-app-bar-nav-icon variant="plain" ripple="false" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+            </v-app-bar>
+        </div>
+
+        <v-navigation-drawer v-model="drawer" location="right">
+            <v-list :items="items"></v-list>
+        </v-navigation-drawer>
+    </v-app>
 
 
-            <v-container class="d-flex justify-center">
-                <div class="d-flex button-group">
-                    <v-btn :ripple="false" variant="plain" to="/atmen"
-                        :class="{ 'active-route': $route.path === '/atmen' }">
-                        <img src="@/assets/navbar_logo_atmen.svg" width="24" height="24">
-                        <div class="btn-text">Atmen</div>
-                    </v-btn>
-                    <v-btn :ripple="false" variant="plain" to="/sprechen"
-                        :class="{ 'active-route': $route.path === '/sprechen' }">
-                        <img src="@/assets/navbar_logo_sprechen.svg" width="24" height="24">
-                        <div class="btn-text">Sprechen</div>
-                    </v-btn>
-                    <v-btn :ripple="false" variant="plain" to="/singen"
-                        :class="{ 'active-route': $route.path === '/singen' }">
-                        <img src="@/assets/navbar_logo_singen.svg" width="24" height="24">
-                        <div class="btn-text">Singen</div>
-                    </v-btn>
-                    <v-btn :ripple="false" variant="plain" to="/klavier"
-                        :class="{ 'active-route': $route.path === '/klavier' }">
-                        <img src="@/assets/navbar_logo_klavier.svg" width="24" height="24">
-                        <div class="btn-text">Klavier</div>
-                    </v-btn>
-                </div>
-            </v-container>
-        </v-app-bar>
-    </div>
 </template>
 
-<script setup>
+<script>
+export default {
+    data: () => ({
+        drawer: false,
+        group: null,
+        items: [
+            {
+                title: 'Über Mich',
+                value: 'foo',
+            },
+            {
+                title: 'Termine & Kontakt',
+                value: 'bar',
+            },
+            {
+                title: 'Youtube',
+                value: 'fizz',
+            },
+            {
+                title: 'Termine',
+                value: 'buzz',
+            },
+        ],
+    }),
 
+    watch: {
+        group() {
+            this.drawer = false
+        },
+    },
+}
 </script>
 
 <style scoped>
@@ -64,7 +108,7 @@
 }
 
 .button-group {
-    gap: 8vw;
+    gap: 6vw;
 }
 
 /* Unterstrich Hover-Effekt */
